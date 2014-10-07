e5b94ef92c1f9f8265d9a4569866f38bdccb24f1
Whitney Young <wbyoung@fadingred.com>
2014-10-07 12:10:41 -0700

    Removed stuff.

--- app.js
+++ app.js
@@ -21,27 +21,3 @@ app.post('/api/people', function(req, res) {
 var server = app.listen(8000, function() {
   console.log('Listening on port %d', server.address().port);
 });
-
-// app.get('/', function(req, res){
-//   res.send('Leslie, Kali, and Steve in da hizzouse');
-// });
-
-// var express = require('express');
-// var app = express();
-
-// app.use(express.static(__dirname + '/public'));
-
-// app.get('/', function(req, res){
-//   res.send('Leslie, Kali, and Steve in da hizzouse');
-// });
-
-// app.get('/api/people', function(req, res){
-// 	// don't use send here
-//   res.send('{ people: [] }');
-// });
-// app.get('/api/people', function(req, res){
-//   // create new person
-// });
-
-// whitney made this comment. it's silly and should be removed.
-// app.listen(3000);
